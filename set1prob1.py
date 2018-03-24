2ff7265a8b600c10c184aea113c1235e6f71912d
magpiewings <camillefassett@gmail.com>
2018-03-24 15:23:58 -0700

    challenge one

---
+++ set1prob1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# convert hex to base64
+# hex: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# base64: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+import binascii
+
+# returns Base64 encoding of a hexidecimal string
+
+
+def hextoBase64(hex):
+    return binascii.b2a_base64(binascii.unhexlify(hex))
